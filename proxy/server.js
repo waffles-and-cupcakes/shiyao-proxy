f5a58c860f707150dcf48f92d6ec16f332a06a07
Shiyao Li <lishiyao221@gmail.com>
2018-04-24 09:50:46 -0700

    proxy server kind of works

--- proxy/server.js
+++ proxy/server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/rooms/:id', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {

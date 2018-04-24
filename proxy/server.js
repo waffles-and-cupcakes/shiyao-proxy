4ae8944f4710c8d7289b84a41f3f0278b849d5dd
Shiyao Li <lishiyao221@gmail.com>
2018-04-23 22:49:30 -0700

    implement shiyao's part proxy

---
+++ proxy/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use('/rooms/:id', express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
